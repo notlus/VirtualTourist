3becd5c61884273e640c6fec9d330b145371fbd5
Jeffrey Sulton <notlus68@me.com>
2015-09-13 18:47:49 -0700

    Add support for adding pins on the map
    * Add UILongPressGestureRecognizer to the storyboard
    
    * Implement handleLongPress to create and an MKPointAnnotation

--- VirtualTourist/TravelLocationsViewController.swift
+++ VirtualTourist/TravelLocationsViewController.swift
@@ -9,18 +9,30 @@
 import UIKit
 import MapKit
 
-class TravelLocationsViewController: UIViewController {
+class TravelLocationsViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func handleLongPress(sender: UILongPressGestureRecognizer) {
+        if sender.state == UIGestureRecognizerState.Began {
+            println("Handling `Began` state for gesture")
+            let touchPoint = sender.locationInView(mapView)
+            let coordinate = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.title = "New pin"
+            annotation.coordinate = coordinate //CLLocationCoordinate2D(latitude: 0, longitude: 0)
+            let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "PinAnnotation")
+            mapView.addAnnotation(annotation)
+        } else
+        {
+            println("Ignoring state")
+        }
     }
-
-
+    
 }
 
