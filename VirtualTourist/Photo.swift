ae18ca675407db16b79aac84cee2ace3ff742aa0
Jeffrey Sulton <notlus68@me.com>
2015-09-17 07:41:47 -0700

    * Add `@objc` to class to make it a valid `NSManagedObject`
    * Create init method that accepts a path

--- VirtualTourist/Photo.swift
+++ VirtualTourist/Photo.swift
@@ -9,10 +9,23 @@
 import Foundation
 import CoreData
 
+@objc(Photo)
 class Photo: NSManagedObject {
     @NSManaged var path: String
     
     override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
         super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    init(path: String, context: NSManagedObjectContext) {
+        guard let entity = NSEntityDescription.entityForName("Photo", inManagedObjectContext: context) else {
+            print("Failed to get the `Photo` entity")
+            fatalError()
+        }
+        
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+        // Initialize properties
+        self.path = path
     }
 }
