8a9063c193e8e56b362bfc5b3b8d483723476ba4
Jeffrey Sulton <notlus68@me.com>
2015-09-18 10:26:41 -0700

    * Set up UICollectionView data source and delegate
    * Fetch photos for a pin (Currently not working)
    
    * Populate cells with images

--- VirtualTourist/PhotoAlbumViewController.swift
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -6,30 +6,92 @@
 //  Copyright (c) 2015 notluS. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
-class PhotoAlbumViewController: UIViewController {
+class PhotoAlbumViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, NSFetchedResultsControllerDelegate {
 
+    var pin: Pin!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        do {
+            try fetchedResultsController.performFetch()
+        }
+        catch {
+            print("Fetch failed")
+            fatalError()
+        }
+        
+        print("fetched \(fetchedResultsController.fetchedObjects!.count) photos")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        if pin!.photos.isEmpty {
+            // Get photos from Flickr
+            print("No photos available")
+//            flickrClient.downloadImagesForLocation(pin!.latitude, longitude: pin!.longitude, storagePath: photosPath) { (error) -> () in
+//                print("Got some imaages")
+//            }
+
+        }
+    }
+
+    // MARK: - Core Data
+    
+    private var sharedContext: NSManagedObjectContext {
+        return CoreDataManager.sharedInstance().managedObjectContext!
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    lazy var fetchedResultsController: NSFetchedResultsController = {
+        
+        let fetchRequest = NSFetchRequest(entityName: "Photo")
+        
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "path", ascending: true)]
+//        fetchRequest.predicate = NSPredicate(format: "pin.latitude == %@ AND pin.longitude == %@", self.pin.latitude, self.pin.longitude)
+        
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+            managedObjectContext: self.sharedContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil)
+        
+        return fetchedResultsController
+    }()
+    
+    // MARK: UICollectionViewDataSource
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchedResultsController.fetchedObjects!.count
     }
-    */
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let reuseIdentifier = "PhotoCell"
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) // as! UICollectionViewCell
+        cell.backgroundColor = UIColor.blueColor()
+        
+        let photo = fetchedResultsController.objectAtIndexPath(indexPath) as! Photo
+        let image = UIImage(contentsOfFile: photo.path)
+        cell.contentView.addSubview(UIImageView(image: image))
+        // Configure the cell
+//        cell.memeImageView.image = appDelegate.memes[indexPath.row].memedImage
+//        cell.backgroundColor = UIColor.blackColor()
+//        cell.delegate = self
+//        cell.deleteButton.hidden = editMode ? false : true
+        
+        return cell
+    }
+    
+    // MARK: UICollectionViewDelegate
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print("Selected cell")
+        // Create the detail view controller
+//        let detailViewController = storyboard!.instantiateViewControllerWithIdentifier("DetailViewController")! as! DetailViewController
+        
+        // Set the index of the selected table view entry
+//        detailViewController.memeIndex = indexPath.row
+//        navigationController!.pushViewController(detailViewController, animated: true)
+    }
 
 }
