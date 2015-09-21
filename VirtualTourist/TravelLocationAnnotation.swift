b5cdc75e27cffd2903f848f95d429baff484027b
Jeffrey Sulton <notlus68@me.com>
2015-09-21 02:33:28 -0700

    Add TravelLocationAnnotation class

---
+++ VirtualTourist/TravelLocationAnnotation.swift
@@ -0,0 +1,15 @@
+//
+//  TravelLocationAnnotation.swift
+//  VirtualTourist
+//
+//  Created by Jeffrey Sulton on 9/21/15.
+//  Copyright © 2015 notluS. All rights reserved.
+//
+
+//import Foundation
+import MapKit
+
+class TravelLocationAnnotation: MKPointAnnotation {
+  
+    var locationPin: Pin?
+}
